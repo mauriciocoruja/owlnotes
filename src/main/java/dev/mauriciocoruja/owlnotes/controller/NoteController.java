a483b52905a955a430a54ca44bf7df3dcb81fe80
mauriciocoruja <mbjr@outlook.com.br>
2022-02-10 23:56:44 -0300

    Implemented Spring Cache with Caffeine as a cache provider

--- src/main/java/dev/mauriciocoruja/owlnotes/controller/NoteController.java
+++ src/main/java/dev/mauriciocoruja/owlnotes/controller/NoteController.java
@@ -1,18 +1,17 @@
 package dev.mauriciocoruja.owlnotes.controller;
 
-import dev.mauriciocoruja.owlnotes.datacache.CacheStore;
 import dev.mauriciocoruja.owlnotes.entities.Note;
 import dev.mauriciocoruja.owlnotes.entities.dto.NoteDTO;
 import dev.mauriciocoruja.owlnotes.entities.util.URL;
 import dev.mauriciocoruja.owlnotes.service.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 @RestController
@@ -20,13 +19,10 @@ import java.util.stream.Collectors;
 public class NoteController {
 
     @Autowired
-    CacheStore<Note> noteCacheStore;
-    @Autowired
-    Logger logger;
-    @Autowired
     private NoteService noteService;
 
     @GetMapping
+    @Cacheable(value = "allNotes")
     public ResponseEntity<List<NoteDTO>> getAllNotes() {
         return ResponseEntity
                 .ok()
@@ -37,22 +33,13 @@ public class NoteController {
     }
 
     @GetMapping("/id/")
+    @Cacheable(value = "noteById")
     public ResponseEntity<Note> getNoteById(@RequestParam String id) {
-
-        Note cachedEmpRecord = noteCacheStore.get(id);
-
-        if (cachedEmpRecord != null) {
-            logger.log(Level.INFO, "Note record found in cache : {0}", noteCacheStore.get(id).getTitle());
-            return ResponseEntity.ok().body(cachedEmpRecord);
-        }
-
-        Note noteFromDataBase = this.noteService.findNoteById(id);
-        noteCacheStore.add(id, noteFromDataBase);
-
-        return ResponseEntity.ok().body(noteFromDataBase);
+        return ResponseEntity.ok().body(this.noteService.findNoteById(id));
     }
 
     @PostMapping("/new-note")
+    @CacheEvict(value = "allNotes")
     public ResponseEntity<NoteDTO> createNote(@RequestBody NoteDTO noteForm) {
         Note noteCreated = this.noteService.createNote(noteForm);
         NoteDTO note = new NoteDTO(noteCreated);
@@ -60,6 +47,7 @@ public class NoteController {
     }
 
     @PutMapping("/update-note")
+    @CacheEvict(cacheNames = {"noteById", "allNotes"})
     public ResponseEntity<NoteDTO> updateNote(@RequestParam String id, @RequestBody NoteDTO noteForm) {
         Note updatedNote = this.noteService.updateNote(id, noteForm);
         NoteDTO note = new NoteDTO(updatedNote);
@@ -67,6 +55,7 @@ public class NoteController {
     }
 
     @DeleteMapping("/delete-note")
+    @CacheEvict(cacheNames = {"noteById", "allNotes"})
     public ResponseEntity<Void> deleteNote(@RequestParam String id) {
         this.noteService.deleteNote(id);
         return ResponseEntity.noContent().build();
